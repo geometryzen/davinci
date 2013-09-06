587544b513fe4f207289f4ca7859fae14783da0e
David Holmes <david.geo.holmes@gmail.com>
2013-09-06 11:08:13 -0400

    Testing CI

--- davinci-deploy.sh
+++ davinci-deploy.sh
@@ -1,6 +1,6 @@
 if [[ "$TRAVIS_PULL_REQUEST" == "false" && "$TRAVIS_TEST_RESULT" == "0" ]]; then
 
-  echo -e "Starting to update distribution folder...\n"
+  echo -e "Starting deployment to distribution.\n"
   # configure Git to commit as Travis
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis"
@@ -57,7 +57,7 @@ if [[ "$TRAVIS_PULL_REQUEST" == "false" && "$TRAVIS_TEST_RESULT" == "0" ]]; then
     cp bower.json ../davinci
     cp .bowerrc ../davinci
     # put the new version in the distribution repository
-    cd ../davinci
+    cd $HOME/davinci
     git add .
     git commit -m "DaVinci version: $TAG"
     git tag $TAG
@@ -65,15 +65,15 @@ if [[ "$TRAVIS_PULL_REQUEST" == "false" && "$TRAVIS_TEST_RESULT" == "0" ]]; then
   done
 
   # reset davinci distribution repository to HEAD just to be sure
-  cd $HOME
-  cd davinci
+  cd $HOME/davinci
   git reset HEAD --hard
-  cd $HOME
 
   # build DaVinci
-  cd davinci-dev
+  cd $HOME/davinci-dev
   git reset HEAD --hard
+
   ./davinci.py dist -u
+
   cd dist
   cp *.js ../../davinci/
 
@@ -88,7 +88,7 @@ if [[ "$TRAVIS_PULL_REQUEST" == "false" && "$TRAVIS_TEST_RESULT" == "0" ]]; then
   git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed"
   git push -fq origin master > /dev/null
 
-  echo -e "Done magic with coverage\n"
+  echo -e "Deployment successful.\n"
 else
-  echo -e "Not updating distribution folder because TRAVIS_PULL_REQUEST = $TRAVIS_PULL_REQUEST and TRAVIS_TEST_RESULT = $TRAVIS_TEST_RESULT"
+  echo -e "Deployment skipped because TRAVIS_PULL_REQUEST = $TRAVIS_PULL_REQUEST and TRAVIS_TEST_RESULT = $TRAVIS_TEST_RESULT"
 fi
